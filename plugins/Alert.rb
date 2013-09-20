f5aaf7aca27ff1097f0b74a5ac056e1792311d0e
rhannequin <remy.hannequin@gmail.com>
2013-09-20 22:54:13 +0200

    Remove bot and ChanServ from alert command - Close #1

--- plugins/Alert.rb
+++ plugins/Alert.rb
@@ -14,6 +14,9 @@ class Alert
   match(/alert (.+)/, method: :execute_alert)
 
   def execute_alert(m, message)
-    m.reply message + ' poke ' + m.channel.get_usernames.join(' ')
+    usernames = m.channel.get_usernames
+    usernames.delete('ChanServ') # Remove ChanServ
+    usernames.delete(m.bot.nick) # Remove bot
+    m.reply message + '   // poke ' + usernames.join(' ')
   end
 end
